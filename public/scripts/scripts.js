01d525f3669102bd29f44e38543b8fa202624398
Moises Miguel <moisesmigueledu@gmail.com>
2017-05-27 22:00:56 -0500

    did more work on it

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,80 +1,77 @@
+
 $(onReady);
-
-
-
+var objectToSend = {};//global variable
 function onReady() {
-    console.log('Inside of onReady');
-    // $('#addButton').on('click', addButttonClicked);
+    console.log('log inside of onReady');
+    //listner for button
+    $(".calculatorNum").on('click', buttonClicked);
+}//end of onReady
 
-    $(".calculatorButton").on('click', calculatorButtonclicked);
-}
+function buttonClicked() {
+    console.log('buttonClicked');
 
-// function addButttonClicked() {
-//     console.log('addButttonClicked was clicked');
-//     console.log($(this).text());
-//     var objectToSend = {
-//         x:$('#firstInput').val(),
-//         y:$('#secondInput').val(),
-//         type:'add'
-//     };//end of objectToSend
-//
-//     console.log(objectToSend);
-//
-//     $.ajax({
-//         type:'POST',
-//         url:'/calculate',
-//         data:objectToSend,
-//         success: function(res) {
-//             console.log('back from server with', res);
-//             console.log(res.calc[res.calc.length - 1].results);
-//             $("#equals").text(res.calc[res.calc.length - 1].results);
-//
-//
-//         }
-//     });//end of ajax
-//
-// }//end of addButttonClicked
+    //adds to the p tag
+    $("#inputField").append($(this).text());
 
-function calculatorButtonclicked(){
-    console.log('calculatorButtonclicked clicked');
+    var x = $("#inputField").text();
+
+    //clears the input field
+    if ($(this).text() === 'Clear') {
+        $("#inputField").empty();
+        
+    }//end of clear if statement
+
+    //checks to see what button was clicked and assings x to the objectToSend
+    if ($(this).text() === '+') {
+        console.log("+ was clicked");
+        // $(this).css('box-shadow', "-10px -10px -5px black");
+        objectToSend.type = 'add';
+
+        x = x.substring(0, x.length-1);
+        objectToSend.x = x;
+        $("#inputField").empty();
+        console.log(x);
+    }//end of add if statement
+    else if ($(this).text() === '-') {
+        console.log('- was clicked');
+        objectToSend.type = 'sub';
+        x = x.substring(0, x.length-1);
+        objectToSend.x = x;
+        $("#inputField").empty();
+    }//end of sub else if statement
+    else if ($(this).text() === 'X') {
+        console.log('X was clicked');
+        objectToSend.type = 'mult';
+        x = x.substring(0, x.length-1);
+        objectToSend.x = x;
+        $("#inputField").empty();
+    }//end of mult else if statement
+    else if ($(this).text() === '/') {
+        console.log('X was clicked');
+        objectToSend.type = 'div';
+        x = x.substring(0, x.length-1);
+        objectToSend.x = x;
+        $("#inputField").empty();
+    }//end of div else if statement
 
 
-
-    var objectToSend = {
-          x:$('#firstInput').val(),
-          y:$('#secondInput').val(),
-
-      };//end of objectToSend
-
-      switch ($(this).text()) {
-        case '+':
-            objectToSend.type = 'add';
-            break;
-        case '-':
-            objectToSend.type = 'sub';
-            break;
-        case 'X':
-            objectToSend.type = 'mult';
-            break;
-        case '/':
-            objectToSend.type = 'div';
-            break;
-         default:
-            console.log('inside of break');
-
-      }//end of switch
+    //sends all the information to the server to presses when the '=' is clicked
+    if($(this).text() === '='){
+        console.log('inside of the = sign');
+        var y = $("#inputField").text();
+        y = y.substring(0, y.length-1);
+        objectToSend.y = y;
+        $("#inputField").empty();
 
         $.ajax({
             type:'POST',
             url:'/calculate',
             data:objectToSend,
             success: function(res) {
-                console.log('back from server with response', res);
-                console.log(res.calc[res.calc.length - 1].results);
-                $("#equals").text(res.calc[res.calc.length - 1].results);
+                console.log('back from the server with ', res);
+                console.log(res.calc[0].result);
+                $("#inputField").text(res.calc[res.calc.length - 1].result);
             }//end of success
         });//end of ajax
-
-
-    console.log(objectToSend);
-}//end of calculatorButtonclicked
+    }//end of if statement
+}//end of button clicked
--- server.js
+++ server.js
@@ -26,36 +26,36 @@ app.post('/calculate', function(req, res){
     var calcObjectToSend = {
         calc:calcArray
     };
-    determinCalc(req);
+    calculateReq(req);
     res.send(calcObjectToSend);
 
 });
 
-function determinCalc(req) {
+function calculateReq(req) {
+    console.log('inside of calculateReq');
     if (req.body.type === 'add') {
-        var add =  Number(req.body.x) + Number(req.body.y);
-        req.body.results = add;
-        console.log(req.body.type);
+        console.log('this is going to add');
+        var add = Number(req.body.x) + Number(req.body.y);
+        req.body.result = add;
         calcArray.push(req.body);
 
     }
     else if (req.body.type === 'sub') {
-        var sub =  Number(req.body.x) - Number(req.body.y);
-        req.body.results = sub;
-        console.log(req.body.type);
+        console.log('server recieved sub');
+        var sub  = Number(req.body.x) - Number(req.body.y);
+        req.body.result = sub;
         calcArray.push(req.body);
     }
     else if (req.body.type === 'mult') {
-        var mult =  Number(req.body.x) * Number(req.body.y);
-        req.body.results = mult;
-        console.log(req.body.type);
+        console.log('server recieved mult');
+        var mult  = Number(req.body.x) * Number(req.body.y);
+        req.body.result = mult;
         calcArray.push(req.body);
     }
     else if (req.body.type === 'div') {
-        var div =  Number(req.body.x) / Number(req.body.y);
-        req.body.results = div;
-        console.log(req.body.type);
+        console.log('server recieved div');
+        var div  = Number(req.body.x) / Number(req.body.y);
+        req.body.result = div;
         calcArray.push(req.body);
     }
-
 }
